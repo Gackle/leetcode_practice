8ca991a19eff2e5315141498ee8e918304b4e505
SinHub <cs12bbaker@outlook.com>
2018-11-11 23:51:21 +0800

    Add new solution 58

---
+++ 58.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+""" 58. 最后一个单词的长度
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+
+示例:
+输入: "Hello World"
+输出: 5
+"""
+
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split(" ")
+        i = len(words) - 1
+        while i >= 0:
+            if words[i] != "":
+                return len(words[i])
+            i -= 1
+        return 0
+
+
+if __name__ == '__main__':
+    s = Solution()
+    S = "Hello  World  "
+    print(s.lengthOfLastWord(S))
